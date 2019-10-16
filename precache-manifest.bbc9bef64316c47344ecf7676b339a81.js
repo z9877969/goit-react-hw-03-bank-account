365187fb76a44e5e40a577133ac63dd94ff05b43
Andriy Kyrylenko <z9877969@ukr.net>
2019-10-16 14:24:15 +0300

    Updates

---
+++ precache-manifest.bbc9bef64316c47344ecf7676b339a81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb404d27ebf6c5cf0e3b3a02e65e5ce4",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "b04132d8dd34aec4f2ed",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "bdaae1097a77cd6b7710",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.76c1f292.chunk.css"
+  },
+  {
+    "revision": "b04132d8dd34aec4f2ed",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.e8fb2dc8.chunk.js"
+  },
+  {
+    "revision": "bdaae1097a77cd6b7710",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.634c5c15.chunk.js"
+  },
+  {
+    "revision": "a6c66774a526f4c37f68",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.ba08dfeb.js"
+  }
+]);
